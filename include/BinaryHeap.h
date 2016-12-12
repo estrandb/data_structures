0d2fef935c3a74f3f29572a579c99370b8900cb2
Eddie <estrandb@gmail.com>
2016-12-11 17:08:09 -0800

    some other stuff

--- include/BinaryHeap.h
+++ include/BinaryHeap.h
@@ -8,21 +8,42 @@ template <typename T>
 class BinaryHeap
 {
     public:
+        //0 == min, 1 == max
         BinaryHeap(int type);
         virtual ~BinaryHeap();
 
-        void insertNode(TreeNode<T> node);
-        void deleteTop();
-        TreeNode<T> peekTop();
-
-
+        void InsertNode(TreeNode<T> node);
+        void DeleteTop();
+        TreeNode<T> GetTop();
 
     protected:
 
     private:
         std::vector<TreeNode<T> > Tree;
-
         const int treeType;
+
+        void BubbleDown(TreeNode<T> node);
+        void BubbleUp(TreeNode<T> node);
+        void Heapify();
 };
+
+template<typename T>
+BinaryHeap<T>::BinaryHeap(int type) : treeType(type)
+{
+    //ctor
+}
+
+template<typename T>
+BinaryHeap<T>::~BinaryHeap()
+{
+    //dtor
+}
+
+template<typename T>
+BinaryHeap<T>::InsertNode(TreeNode<T> node)
+{
+
+}
+
 
 #endif // BINARYHEAP_H
