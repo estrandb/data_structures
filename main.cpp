28eaba385a048a12d9710f0be11660a81a8a19a6
Eddie <estrandb@gmail.com>
2016-12-16 10:07:10 -0800

    heap seems to work with node templates

--- main.cpp
+++ main.cpp
@@ -8,5 +8,15 @@ int main()
 {
     auto binheap = new BinaryHeap<AdjListNode>(0);
     auto node = new AdjListNode();
+    auto treeNode = new TreeNode<AdjListNode>();
+
+    node->dest[0] = 1;
+    node->weight[0] = 20;
+
+    treeNode->data = *node;
+    treeNode->sortByValue = node->dest[0];
+
+    binheap->InsertNode(*treeNode);
+
     return 0;
 }
